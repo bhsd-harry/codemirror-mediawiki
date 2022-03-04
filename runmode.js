1f427150bdbf8f11e42ce15f0e13f1f431dbbb17
bhsd <2545473905@qq.com>
2022-03-04 11:10:33 -0800

    feat: update runmode

--- runmode.js
+++ runmode.js
@@ -1,14 +1,30 @@
 'use strict';
 
 const fs = require( 'fs' ),
-	CodeMirror = require( './node_modules/codemirror/addon/runmode/runmode.node.js' ),
 	text = fs.readFileSync( 'test.txt', 'utf8' );
+global.CodeMirror = require( './node_modules/codemirror/addon/runmode/runmode.node.js' );
 require( './test.js' );
 CodeMirror.defaults.mwConfig = require( './config.json' );
-CodeMirror.runMode( text, 'text/mediawiki', callback );
-
-function callback( string, style, line, ch ) {
-	if ( /\berror\b/.test( style ) ) {
-		console.log( { string: string, line: line, ch: ch } );
+var output = '',
+	lastStyle;
+const callback = {
+	error: function ( string, style, line, ch ) {
+		if ( /\berror\b/.test( style ) ) {
+			console.log( { string: string, line: line, ch: ch } );
+		}
+	},
+	print: function ( string, style ) {
+		if ( string === '\n' ) {
+			console.log( output );
+			output = '';
+		} else if ( string.length ) {
+			style = ( style || '' ).trim().replace( /\bmw-/g, '' );
+			if ( lastStyle === style ) {
+				output = output.slice( 0, -style.length - 11 );
+			}
+			output += string + '\x1b[32m{' + style + '}\x1b[0m';
+			lastStyle = style;
+		}
 	}
-}
+};
+CodeMirror.runMode( text, 'text/mediawiki', callback[ process.argv[ 2 ] || 'error' ] );
