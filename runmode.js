3d9849a081066ce4ef3dae5b368b850f99a8ca52
bhsd <2545473905@qq.com>
2022-04-09 23:25:44 -0700

    build: clean up unused dependency

--- runmode.js
+++ runmode.js
@@ -4,7 +4,7 @@
 const fs = require( 'fs' ),
 	text = fs.readFileSync( 'test.txt', 'utf8' ),
 	{ argv: [ ,, mode = 'error' ] } = process;
-global.CodeMirror = require( './node_modules/codemirror/addon/runmode/runmode.node.js' );
+global.CodeMirror = require( './codemirror' );
 require( './test.js' );
 CodeMirror.defaults.mwConfig = require( './config.json' );
 CodeMirror.errorMsgs = require( './i18n/zh-hans.json' );
@@ -88,13 +88,16 @@ const callback = {
 		lastStyle = style === undefined ? lastStyle : style;
 	},
 };
+if ( !( mode in callback ) ) {
+	throw new RangeError( '未定义的运行模式！' );
+}
 CodeMirror.runMode( text, 'text/mediawiki', callback[ mode ] );
 if ( mode === 'error' ) {
 	console.log( `还有 \x1b[1;31m${ finalState.nExt }\x1b[0m 个解析器函数` );
 	console.log( `　　 \x1b[1;31m${ finalState.nTemplate }\x1b[0m 个模板` );
 	console.log( `　　 \x1b[1;31m${ finalState.nLink }\x1b[0m 个链接` );
 	console.log( `　　 \x1b[1;31m${ finalState.InHtmlTag.length }\x1b[0m 个HTML标签` );
-	console.log( `　　 \x1b[1;31m${ finalState.nInvisible }\x1b[0m 个不可见元素未闭合。` );
+	console.log( `　　 \x1b[1;31m${ finalState.nInvisible || 0 }\x1b[0m 个不可见元素未闭合。` );
 } else {
 	console.log( output );
 }
