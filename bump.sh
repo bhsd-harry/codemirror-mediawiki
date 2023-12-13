bd074ae7b068a6ae1413302480321aec3a47ba46
bhsd <2545473905@qq.com>
2023-12-13 21:02:30 +0800

    feat(bump.sh): update

--- bump.sh
+++ bump.sh
@@ -1,6 +1,19 @@
 #!/usr/local/bin/bash
-git add -A
-git commit -m "chore: bump version to $1"
-git push
-git tag $1
-git push origin $1
+if [[ $2 == 'npm' ]]
+then
+	npm run build
+	sed -i '' -E "s/\"version\": \".+\"/\"version\": \"$1\"/" package.json
+	git add -A
+	git commit -m "chore: publish $1 to npm"
+	npm publish --tag ${3-latest}
+else 
+	npm run lint
+	if [[ $? -eq 0 ]]
+	then
+		git add -A
+		git commit -m "chore: bump version to $1"
+		git push
+		git tag $1
+		git push origin $1
+	fi
+fi
