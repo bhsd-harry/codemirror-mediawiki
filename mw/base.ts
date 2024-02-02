20fecaa77ffeab6bd3aab2718c206b1f6fdd43c0
bhsd <2545473905@qq.com>
2024-02-02 22:58:52 +0800

    chore: publish 2.3.1 to npm

--- mw/base.ts
+++ mw/base.ts
@@ -1,4 +1,4 @@
-import {CodeMirror6, CDN} from 'https://testingcf.jsdelivr.net/npm/@bhsd/codemirror-mediawiki@2.3.0/dist/main.min.js';
+import {CodeMirror6, CDN} from 'https://testingcf.jsdelivr.net/npm/@bhsd/codemirror-mediawiki@2.3.1/dist/main.min.js';
 import {getMwConfig, USING_LOCAL} from './config';
 import {openLinks, pageSelector} from './openLinks';
 import {instances, textSelection} from './textSelection';
