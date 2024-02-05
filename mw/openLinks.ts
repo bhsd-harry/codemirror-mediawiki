923c1d65cc04933f376b21ff663ca0d849fcfbd7
bhsd <2545473905@qq.com>
2024-02-06 04:46:38 +0800

    refactor(openLinks): get rid of event delegation

--- mw/openLinks.ts
+++ mw/openLinks.ts
@@ -1,43 +1,45 @@
 import {isMac} from './msg';
 import type {CodeMirror} from './base';
 
+declare type MouseEventListener = (e: MouseEvent) => void;
+
 const modKey = isMac ? 'metaKey' : 'ctrlKey',
-	pageSelector = '.cm-mw-template-name, .cm-mw-link-pagename',
 	regex = /-template-name|link-pagename/u,
-	handlers = new WeakMap<CodeMirror, JQuery.EventHandlerBase<HTMLElement, JQuery.ClickEvent>>();
+	handlers = new WeakMap<CodeMirror, MouseEventListener>();
 
 /**
  * 点击时在新页面打开链接、模板等
  * @param cm CodeMirror实例
  * @param e 点击事件
  */
-const getHandler = (cm: CodeMirror): JQuery.EventHandlerBase<HTMLElement, JQuery.ClickEvent> => {
+const getHandler = (cm: CodeMirror): MouseEventListener => {
 	if (handlers.has(cm)) {
 		return handlers.get(cm)!;
 	}
-	const handler: JQuery.EventHandlerBase<HTMLElement, JQuery.ClickEvent> = function(this, e): void {
+	const handler: MouseEventListener = (e): void => {
 		if (!e[modKey]) {
 			return;
 		}
 		e.preventDefault();
 		const {view} = cm,
-			node = cm.getNodeAt(view.posAtCoords({x: e.clientX, y: e.clientY})!);
-		if (!node) {
+			node = cm.getNodeAt(view.posAtCoords(e)!);
+		if (!node || !regex.test(node.name)) {
 			return;
 		}
+		const {name} = node;
 		let prevSibling = node,
 			nextSibling = node;
-		while (regex.test(prevSibling.prevSibling?.name || '')) {
+		while (prevSibling.prevSibling?.name === name) {
 			prevSibling = prevSibling.prevSibling!;
 		}
-		while (regex.test(nextSibling.nextSibling?.name || '')) {
+		while (nextSibling.nextSibling?.name === name) {
 			nextSibling = nextSibling.nextSibling!;
 		}
-		let page = view.state.sliceDoc(prevSibling.from, nextSibling.to);
+		let page = view.state.sliceDoc(prevSibling.from, nextSibling.to).trim();
 		if (page.startsWith('/')) {
 			page = `:${mw.config.get('wgPageName')}${page}`;
 		}
-		open(new mw.Title(page, this.classList.contains('cm-mw-template-name') ? 10 : 0).getUrl(undefined), '_blank');
+		open(new mw.Title(page, name.includes('-template-name') ? 10 : 0).getUrl(undefined), '_blank');
 	};
 	handlers.set(cm, handler);
 	return handler;
@@ -49,10 +51,13 @@ const getHandler = (cm: CodeMirror): JQuery.EventHandlerBase<HTMLElement, JQuery
  * @param on 是否添加
  */
 export const openLinks = (cm: CodeMirror, on?: boolean): void => {
+	const {view: {contentDOM}} = cm;
 	if (on) {
 		mw.loader.load('mediawiki.Title');
-		$(cm.view.contentDOM).on('click', pageSelector, getHandler(cm)).css('--codemirror-cursor', 'pointer');
+		contentDOM.addEventListener('click', getHandler(cm));
+		contentDOM.style.setProperty('--codemirror-cursor', 'pointer');
 	} else if (on === false) {
-		$(cm.view.contentDOM).off('click', pageSelector, getHandler(cm)).css('--codemirror-cursor', '');
+		contentDOM.removeEventListener('click', getHandler(cm));
+		contentDOM.style.removeProperty('--codemirror-cursor');
 	}
 };
