81bbec20104002950939a9395e40713141281812
bhsd <2545473905@qq.com>
2024-02-21 10:16:46 +0800

    chore: publish 2.5.0 to npm

--- mw/base.ts
+++ mw/base.ts
@@ -1,4 +1,4 @@
-import {CodeMirror6, CDN} from 'https://testingcf.jsdelivr.net/npm/@bhsd/codemirror-mediawiki@2.4.7/dist/main.min.js';
+import {CodeMirror6, CDN} from 'https://testingcf.jsdelivr.net/npm/@bhsd/codemirror-mediawiki@2.5.0/dist/main.min.js';
 import {getMwConfig, getParserConfig} from './config';
 import {openLinks} from './openLinks';
 import {instances, textSelection} from './textSelection';
--- mw/msg.ts
+++ mw/msg.ts
@@ -1,6 +1,6 @@
 import type {CodeMirror} from './base';
 
-export const REPO_CDN = 'npm/@bhsd/codemirror-mediawiki@2.4.7';
+export const REPO_CDN = 'npm/@bhsd/codemirror-mediawiki@2.5.0';
 
 const {vendor, userAgent, maxTouchPoints, platform} = navigator;
 
