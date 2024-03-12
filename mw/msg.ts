ec20f5a57995407096f0a6d48a5326569b7f0a24
bhsd <2545473905@qq.com>
2024-03-12 13:54:42 +0800

    chore: publish 2.8.0 to npm

--- mw/msg.ts
+++ mw/msg.ts
@@ -1,6 +1,6 @@
 import type {CodeMirror} from './base';
 
-export const REPO_CDN = 'npm/@bhsd/codemirror-mediawiki@2.7.7',
+export const REPO_CDN = 'npm/@bhsd/codemirror-mediawiki@2.8.0',
 	curVersion = REPO_CDN.slice(REPO_CDN.lastIndexOf('@') + 1);
 
 const {vendor, userAgent, maxTouchPoints, platform} = navigator;
