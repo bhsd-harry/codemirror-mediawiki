862eec198775fb66daef91fab5dc02783836521c
bhsd <2545473905@qq.com>
2024-04-01 12:52:19 +0800

    chore: publish 2.9.2 to npm

--- mw/base.ts
+++ mw/base.ts
@@ -1,4 +1,4 @@
-import {CodeMirror6, CDN} from 'https://testingcf.jsdelivr.net/npm/@bhsd/codemirror-mediawiki@2.9.1/dist/main.min.js';
+import {CodeMirror6, CDN} from 'https://testingcf.jsdelivr.net/npm/@bhsd/codemirror-mediawiki@2.9.2/dist/main.min.js';
 import {getMwConfig, getParserConfig} from './config';
 import {openLinks} from './openLinks';
 import {instances, textSelection} from './textSelection';
