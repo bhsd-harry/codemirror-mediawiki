5479206492f7643460c9a162b339276f500aec17
bhsd <2545473905@qq.com>
2024-07-22 00:03:03 +0800

    test: more parser tests

--- test/test.ts
+++ test/test.ts
@@ -1,63 +1,51 @@
 import * as fs from 'fs';
-import * as path from 'path';
+import * as parserTests from 'wikiparser-node/test/parserTests.json';
 import parser from './parser';
 
 declare interface Token {
 	text: string;
 	name: string;
 }
+declare interface Test {
+	desc: string;
+	wikitext?: string;
+	parsed?: string;
+	html?: string;
+	print?: string;
+	render?: string;
+}
 
-const tests: {desc: string, wikitext?: string, parsed?: string}[] = [],
-	entities = {'<': '&lt;', '>': '&gt', '&': '&amp;'},
-	cwd = '../wikiparser-node/test/core',
-	files = new Set(fs.readdirSync(`${cwd}/`));
-files.delete('parserTests.txt');
-for (const file of ['parserTests.txt', ...files]) {
-	tests.push({desc: file.slice(0, -4)});
-	const content = fs.readFileSync(fs.realpathSync(path.join(cwd, file)), 'utf8'),
-		// eslint-disable-next-line es-x/no-string-prototype-matchall
-		cases = [...content.matchAll(/^!!\s*test\n.+?^!!\s*end$/gmsu)],
-		re = /^!!\s*options(?:\n(?:parsoid=wt2html.*|(?:(?:subpage )?title|preprocessor|thumbsize)=.+|language=(?:en|zh) .*|cat|subpage|showindicators|djvu|showmedia|showtocdata))*\n!/mu;
-	for (const [test] of cases) {
-		if (
-			/^!!\s*html(?:\/(?:php|\*))?$/mu.test(test)
-			&& (!test.includes('options') || re.test(test))
-		) {
-			try {
-				const wikitext = /^!!\s*wikitext\n+((?!!!)[^\n].*?)^!!/msu.exec(test)?.[1]!.trimEnd(),
-					html = /^!!\s*html(?:\/(?:php|\*))?\n(.*?)^!!/msu.exec(test)![1]!.trim(),
-					desc = /^!!\s*test\n(.*?)\n!!/msu.exec(test)![1]!;
-				if (
-					wikitext
-					&& !/\b(?:NULL\b|array\s*\()/u.test(html)
-					&& !/<(?:span|static|aside)tag\b/iu.test(wikitext)
-				) {
-					let node = parser.parse(wikitext).topNode.firstChild;
-					const tokens: Token[] = [];
-					while (node) {
-						const {from, to} = node,
-							name = node.name.replace(/_+/gu, ' ').trim().replace(/mw-/gu, ''),
-							last = tokens[tokens.length - 1];
-						if (last?.name === name) {
-							last.text += wikitext.slice(from, to);
-						} else {
-							tokens.push({text: wikitext.slice(from, to), name});
-						}
-						node = node.nextSibling;
-					}
-					tests.push({
-						desc,
-						wikitext,
-						parsed: tokens.map(({name, text}) => {
-							const escaped = text.replace(/[<>&]/gu, m => entities[m as '<' | '>' | '&']);
-							return name ? `<${name}>${escaped}</>` : text;
-						}).join(''),
-					});
+const tests: Test[] = parserTests,
+	entities = {'<': '&lt;', '>': '&gt', '&': '&amp;'};
+for (let i = tests.length - 1; i >= 0; i--) {
+	const test = tests[i]!,
+		{wikitext} = test;
+	if (wikitext) {
+		try {
+			let node = parser.parse(wikitext).topNode.firstChild;
+			const tokens: Token[] = [];
+			while (node) {
+				const {from, to} = node,
+					name = node.name.replace(/_+/gu, ' ').trim().replace(/mw-/gu, ''),
+					last = tokens[tokens.length - 1];
+				if (last?.name === name) {
+					last.text += wikitext.slice(from, to);
+				} else {
+					tokens.push({text: wikitext.slice(from, to), name});
 				}
-			} catch (e) {
-				console.error(test);
-				throw e;
+				node = node.nextSibling;
 			}
+			delete test.html;
+			delete test.print;
+			delete test.render;
+			test.parsed = tokens.map(({name, text}) => {
+				const escaped = text.replace(/[<>&]/gu, m => entities[m as '<' | '>' | '&']);
+				return name ? `<${name}>${escaped}</>` : text;
+			}).join('');
+		} catch (e) {
+			console.error(test);
+			tests.splice(i, 1);
+			throw e;
 		}
 	}
 }
