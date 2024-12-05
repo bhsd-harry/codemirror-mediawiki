3a60c6c104a1dfb2a2cb9697bec47e0af32ccc7c
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2024-12-05 21:18:37 +0800

    chore: bump version to 2.19.2

--- bump.sh
+++ bump.sh
@@ -1,22 +1,16 @@
 #!/usr/local/bin/bash
 if [[ $2 == 'npm' ]]
 then
+	npm run build && npm publish --tag ${3-latest}
+else 
 	sed -i '' -E "s|codemirror-mediawiki@[^/']+|codemirror-mediawiki@$1|g" mw/msg.ts
-	npm run build
+	npm run lint && npm run build:test && npm run test:real
 	if [[ $? -eq 0 ]]
 	then
 		for x in i18n/* package.json
 		do
 			sed -i '' -E "s/\"version\": \".+\"/\"version\": \"$1\"/" $x
 		done
-		git add -A
-		git commit -m "chore: publish $1 to npm"
-		npm publish --tag ${3-latest}
-	fi
-else 
-	npm run lint && npm run build:test && npm run test:real
-	if [[ $? -eq 0 ]]
-	then
 		git add -A
 		git commit -m "chore: bump version to $1"
 		git push
