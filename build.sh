7620bfb6ebe1e0f4f765aff6fd4423608d17b2b3
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2025-01-24 11:19:39 +0800

    fix(build.sh): shebang

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+#!/usr/local/bin/bash
 printf '%s\n%s' '// @ts-nocheck' "$(cat src/gh-page.ts)" > src/temp-1.ts
 printf '%s\n%s' '// @ts-nocheck' "$(cat src/test-page.ts)" > src/temp-2.ts
 tsc --project tsconfig.gh-page.json
