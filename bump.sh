8c91e88674c24107c41e08cfe00162a6a075de03
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2025-03-08 22:42:25 +0800

    fix(bump.sh): double quotes

--- bump.sh
+++ bump.sh
@@ -1,7 +1,7 @@
 #!/usr/local/bin/bash
 if [[ $2 == 'npm' ]]
 then
-	npm publish --tag ${3-latest}
+	npm publish --tag "${3-latest}"
 else
 	sed -i '' -E "s|codemirror-mediawiki@[^/']+|codemirror-mediawiki@$1|g" mw/msg.ts
 	npm run lint && npm run build:test && npm run test:real && npm run build && npm run build:gh-page
@@ -9,12 +9,12 @@ else
 	then
 		for x in i18n/* package.json
 		do
-			sed -i '' -E "s/\"version\": \".+\"/\"version\": \"$1\"/" $x
+			sed -i '' -E "s/\"version\": \".+\"/\"version\": \"$1\"/" "$x"
 		done
 		git add -A
 		git commit -m "chore: bump version to $1"
 		git push
-		git tag $1
-		git push origin $1
+		git tag "$1"
+		git push origin "$1"
 	fi
 fi
