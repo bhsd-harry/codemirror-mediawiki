d33c6983e393805312f38fc27f8026bd6a09ea2a
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2025-06-24 13:11:20 +0800

    chore: bump version to 2.29.0

--- mw/msg.ts
+++ mw/msg.ts
@@ -4,7 +4,7 @@ import type {CodeMirror} from './codemirror';
 
 const storageKey = 'codemirror-mediawiki-i18n';
 
-export const REPO_CDN = 'npm/@bhsd/codemirror-mediawiki@2.28.2',
+export const REPO_CDN = 'npm/@bhsd/codemirror-mediawiki@2.29.0',
 	curVersion = REPO_CDN.slice(REPO_CDN.lastIndexOf('@') + 1),
 	// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
 	languages = mw.language?.getFallbackLanguageChain() ?? [mw.config.get('wgUserLanguage')],
