7795f41243c46ce60d36076cb3cb8f3992683101
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2025-07-04 17:55:46 +0800

    chore: bump version to 2.30.1

--- mw/msg.ts
+++ mw/msg.ts
@@ -4,7 +4,7 @@ import type {CodeMirror} from './codemirror';
 
 const storageKey = 'codemirror-mediawiki-i18n';
 
-export const REPO_CDN = 'npm/@bhsd/codemirror-mediawiki@2.30.0',
+export const REPO_CDN = 'npm/@bhsd/codemirror-mediawiki@2.30.1',
 	curVersion = REPO_CDN.slice(REPO_CDN.lastIndexOf('@') + 1),
 	languages = (async () => {
 		await mw.loader.using('mediawiki.language');
