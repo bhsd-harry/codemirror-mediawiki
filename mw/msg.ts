a0fee54ed407dbf348878888ab32f78f428b2ba4
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2025-08-05 22:40:46 +0800

    chore: bump version to 3.0.1

--- mw/msg.ts
+++ mw/msg.ts
@@ -6,7 +6,7 @@ declare const $LANGS: string[];
 
 const storageKey = 'codemirror-mediawiki-i18n';
 
-export const REPO_CDN = 'npm/@bhsd/codemirror-mediawiki@3.0.0',
+export const REPO_CDN = 'npm/@bhsd/codemirror-mediawiki@3.0.1',
 	curVersion = REPO_CDN.slice(REPO_CDN.lastIndexOf('@') + 1),
 	languages = (async () => {
 		await mw.loader.using('mediawiki.language');
