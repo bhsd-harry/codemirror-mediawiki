da6210f08924a7f9c07ae911fdf7369e56676a7d
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2026-04-23 23:45:18 +0800

    chore(package.json): update

--- src/color.ts
+++ src/color.ts
@@ -1,52 +1,37 @@
-import {splitColors, numToHex} from '@bhsd/common';
-import {
-	parseCallExpression,
-	parseColorLiteral,
-	ColorType,
-	colorPickerTheme,
-	makeColorPicker,
-} from '@bhsd/codemirror-css-color-picker';
+import {splitColors} from '@bhsd/common';
+import {parseCallExpression, parseColorLiteral, makeColorPicker} from '@bhsd/codemirror-css-color-picker';
 import type {Extension} from '@codemirror/state';
-import type {WidgetOptions} from '@bhsd/codemirror-css-color-picker';
+import type {WidgetOptions, DiscoverColors} from '@bhsd/codemirror-css-color-picker';
 
-export const discoverColors: Parameters<typeof makeColorPicker>[0]['discoverColors'] = (_, from, to, type, doc) => {
+export const discoverColors: DiscoverColors = (_, {from, to, name}, doc) => {
 	if (
-		!/mw-(?:(?:ext|html)tag-attribute-value|table-definition)/u.test(type)
+		!/mw-(?:(?:ext|html)tag-attribute-value|table-definition)/u.test(name)
 		&& (
-			!/mw-(?:template|parserfunction)(?:$|_)/u.test(type)
+			!/mw-(?:template|parserfunction)(?:$|_)/u.test(name)
 			|| !/[|=]/u.test(doc.sliceString(from - 1, from))
 			|| !/[|\n]/u.test(doc.sliceString(to, to + 1)) && doc.sliceString(to, to + 2) !== '}}'
 		)
 		&& (
-			!/mw-templatevariable(?:$|_)/u.test(type)
+			!/mw-templatevariable(?:$|_)/u.test(name)
 			|| doc.sliceString(from - 1, from) !== '|'
 			|| doc.sliceString(to, to + 1) !== '|' && doc.sliceString(to, to + 3) !== '}}}'
 		)
 	) {
-		return null;
+		return undefined;
 	}
 	return splitColors(doc.sliceString(from, to)).filter(([,,, isColor]) => isColor)
-		.map(([s, start, end]): WidgetOptions | null => {
+		.map(([s, start, end]): WidgetOptions | false | undefined => {
 			const color = s.startsWith('#') ? parseColorLiteral(s) : parseCallExpression(s);
-			let alpha = color?.alpha;
-			if (color?.colorType !== ColorType.hex) {
-				alpha &&= numToHex(parseFloat(alpha.slice(1)) / (alpha.endsWith('%') ? 100 : 1));
-			}
 			return color && {
 				...color,
-				colorType: ColorType.hex,
-				alpha: alpha ?? '',
 				from: from + start,
 				to: from + end,
 			};
-		}).filter(options => options !== null);
+		}).filter(Boolean) as WidgetOptions[];
 };
 
 /**
  * Get the [colorPicker](https://github.com/bhsd-harry/codemirror-mediawiki/tree/wikitext#colorpicker)
  * extension for Wikitext.
  */
-export default (): Extension => [
-	makeColorPicker({discoverColors}),
-	colorPickerTheme,
-];
+export default (): Extension => makeColorPicker(discoverColors);
