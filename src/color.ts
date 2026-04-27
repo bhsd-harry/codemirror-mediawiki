62bc058c6800f5f2dc7a4e0c7d651f2be05a7196
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2026-04-27 18:23:45 +0800

    refactor(color): if-condition

--- src/color.ts
+++ src/color.ts
@@ -6,31 +6,30 @@ import type {WidgetOptions, DiscoverColors} from '@bhsd/codemirror-css-color-pic
  * @implements
  * @test
  */
-export const discoverColors: DiscoverColors = (_, {from, to, name}, doc) => {
-	if (
-		!/mw-(?:(?:ext|html)tag-attribute-value|table-definition)/u.test(name)
-		&& (
-			!/mw-(?:template|parserfunction)(?:$|_)/u.test(name)
-			|| !/[|=]/u.test(doc.sliceString(from - 1, from))
-			|| !/[|\n]/u.test(doc.sliceString(to, to + 1)) && doc.sliceString(to, to + 2) !== '}}'
-		)
-		&& (
-			!/mw-templatevariable(?:$|_)/u.test(name)
-			|| doc.sliceString(from - 1, from) !== '|'
-			|| doc.sliceString(to, to + 1) !== '|' && doc.sliceString(to, to + 3) !== '}}}'
-		)
-	) {
-		return undefined;
-	}
-	return splitColors(doc.sliceString(from, to)).filter(([,,, isColor]) => isColor)
-		.map(([s, start, end]): WidgetOptions | false | undefined => {
-			const color = s.startsWith('#') ? parseColorLiteral(s) : parseCallExpression(s);
-			return color && {
-				...color,
-				from: from + start,
-				to: from + end,
-			};
-		}).filter(Boolean) as WidgetOptions[];
-};
+export const discoverColors: DiscoverColors = (_, {from, to, name}, doc) =>
+	// HTML tag attribute values, including wikitext tables
+	/mw-(?:(?:ext|html)tag-attribute-value|table-definition)/u.test(name)
+
+	// template and parser function arguments, immediately following a pipe or equals sign,
+	// and immediately preceding a pipe, newline, or closing braces
+	|| /mw-(?:template|parserfunction)(?:$|_)/u.test(name)
+	&& /[|=]/u.test(doc.sliceString(from - 1, from))
+	&& (/[|\n]/u.test(doc.sliceString(to, to + 1)) || doc.sliceString(to, to + 2) === '}}')
+
+	// variable defaults, immediately following a pipe, and immediately preceding a pipe or closing braces
+	|| /mw-templatevariable(?:$|_)/u.test(name)
+	&& doc.sliceString(from - 1, from) === '|'
+	&& (doc.sliceString(to, to + 1) === '|' || doc.sliceString(to, to + 3) === '}}}')
+
+		? splitColors(doc.sliceString(from, to)).filter(([,,, isColor]) => isColor)
+			.map(([s, start, end]): WidgetOptions | false | undefined => {
+				const color = s.startsWith('#') ? parseColorLiteral(s) : parseCallExpression(s);
+				return color && {
+					...color,
+					from: from + start,
+					to: from + end,
+				};
+			}).filter(Boolean) as WidgetOptions[]
+		: undefined;
 
 export default makeColorPicker(discoverColors);
