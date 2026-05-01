b3bf86a9e38ea934781a7b18400a570550014c0b
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2026-05-01 12:03:21 +0800

    fix(blockCursor): block cursor position

--- src/blockCursor.ts
+++ src/blockCursor.ts
@@ -1,12 +1,14 @@
 import {EditorView} from '@codemirror/view';
+import {EditorSelection} from '@codemirror/state';
 import type {Extension} from '@codemirror/state';
 
 const cursorWidth = '--cursor-width',
 	cursorMargin = '--cursor-margin';
 
 export default (): Extension => [
-	EditorView.updateListener.of(({view, state: {selection: {main: {head}}}, docChanged, selectionSet}) => {
+	EditorView.updateListener.of(({view, state: {selection}, docChanged, selectionSet, transactions}) => {
 		if (docChanged || selectionSet) {
+			const {head} = selection.main;
 			let width = '1ch';
 			if (view.lineBlockAt(head).to !== head) {
 				const rect = view.coordsForChar(head);
@@ -23,6 +25,16 @@ export default (): Extension => [
 					? '0'
 					: `-${width}`,
 			);
+
+			// 避免在行尾换行处显示块状光标
+			if (!transactions.some(tr => tr.isUserEvent('select.blockCursor'))) {
+				view.dispatch({
+					selection: EditorSelection.create(
+						selection.ranges.map(r => r.empty ? EditorSelection.cursor(r.head, 1) : r),
+					),
+					userEvent: 'select.blockCursor',
+				});
+			}
 		}
 	}),
 	EditorView.baseTheme({
