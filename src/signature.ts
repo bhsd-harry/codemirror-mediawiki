bfdb0927ba97b291b997b506955d53f3b35bf915
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2026-07-12 03:26:12 +0800

    refactor(signature): import `getSignatureHelpExtension()`

--- src/signature.ts
+++ src/signature.ts
@@ -1,63 +1,17 @@
-import {EditorView, showTooltip} from '@codemirror/view';
-import {StateField, StateEffect} from '@codemirror/state';
-import {getLSP} from '@bhsd/browser';
-import {createTooltipView} from '@bhsd/cm-util';
+import {getLSP, escHTML} from '@bhsd/browser';
+import {getSignatureHelpExtension} from '@bhsd/cm-util/cm';
 import {baseData, hoverSelector} from './constants.js';
 import {hoverStyle} from './hover.js';
 import {
 	indexToPos,
-	escHTML,
 	toConfigGetter,
 	updateCDN,
 } from './util.js';
-import type {TooltipView, Tooltip} from '@codemirror/view';
 import type {Extension} from '@codemirror/state';
 import type {
 	SignatureHelp,
 } from 'vscode-languageserver-types';
 import type {ConfigData} from 'wikiparser-node';
-
-declare interface SignatureEffect {
-	signatureHelp?: SignatureHelp | undefined;
-	text: string;
-	cursor: number;
-}
-
-const signatureEffect = StateEffect.define<SignatureEffect>(),
-	signatureField = StateField.define<SignatureEffect | undefined>({
-		create() {
-			return undefined;
-		},
-		update(oldValue, {state: {doc, selection: {main: {head}}}, effects}) {
-			const text = doc.toString();
-			for (const effect of effects) {
-				if (effect.is(signatureEffect)) {
-					const {value} = effect;
-					if (head === value.cursor && text === value.text) {
-						return value;
-					}
-				}
-			}
-			return oldValue;
-		},
-		provide(f) {
-			return showTooltip.from(f, (value): Tooltip | null => {
-				if (!value) {
-					return null;
-				}
-				const {cursor, signatureHelp} = value;
-				return signatureHelp?.signatures.length
-					? {
-						pos: cursor,
-						above: true,
-						create(view): TooltipView {
-							return createTooltipView(view, getSignatureHelp(signatureHelp), hoverSelector.slice(1));
-						},
-					}
-					: null;
-			});
-		},
-	});
 
 export const getSignatureHelp = ({signatures, activeParameter: active}: SignatureHelp): string =>
 	signatures.map(signature => {
@@ -72,30 +26,18 @@ export const getSignatureHelp = ({signatures, activeParameter: active}: Signatur
 		return `${safeLabel.slice(0, colon)}:${parts.join('|')}}}`;
 	}).join('<br>');
 
-const dispatchSignatureEffect = (view: EditorView, effect: SignatureEffect): void => {
-	view.dispatch({
-		effects: signatureEffect.of(effect),
-	});
-};
-
 export default (
 	configData: ConfigData,
 	cdn?: string,
 ): Extension => {
 	updateCDN(cdn);
 	return [
-		signatureField,
-		EditorView.updateListener.of(({view, state, docChanged, selectionSet}) => {
-			if (docChanged || selectionSet && state.field(signatureField)?.signatureHelp?.signatures.length) {
-				const {doc, selection: {main}} = state,
-					{head: cursor} = main,
-					text = doc.toString();
-				if (!main.empty) {
-					dispatchSignatureEffect(view, {text, cursor});
-					return;
-				}
-				(async () => {
-					const lsp = getLSP(
+		getSignatureHelpExtension<SignatureHelp>({
+			className: hoverSelector.slice(1),
+			render: getSignatureHelp,
+			async update(view, state, {text, cursor}) {
+				const lsp =
+					getLSP(
 						view,
 						true,
 						{
@@ -105,21 +47,12 @@ export default (
 							cdn: baseData.CDN,
 						},
 					);
-					// eslint-disable-next-line prefer-const
-					let signatureHelp: SignatureHelp | undefined = await lsp?.provideSignatureHelp(
-						text,
-						indexToPos(doc, cursor),
-					);
-					dispatchSignatureEffect(view, {text, cursor, signatureHelp});
-				})();
-			}
-		}),
-		EditorView.domEventHandlers({
-			keydown({key}, view) {
-				if (key === 'Escape') {
-					const {doc, selection: {main: {head}}} = view.state;
-					dispatchSignatureEffect(view, {text: doc.toString(), cursor: head});
-				}
+				// eslint-disable-next-line prefer-const
+				let signatureHelp: SignatureHelp | undefined = await lsp?.provideSignatureHelp(
+					text,
+					indexToPos(state.doc, cursor),
+				);
+				return signatureHelp;
 			},
 		}),
 		hoverStyle,
