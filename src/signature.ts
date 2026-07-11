58ba5915af572caec2cd7e3d6b79f37ca58a3d24
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2026-07-11 13:30:49 +0800

    refactor(signature): import `getSignatureHelpExtension()`

--- src/signature.ts
+++ src/signature.ts
@@ -1,8 +1,6 @@
-import {EditorView, showTooltip} from '@codemirror/view';
-import {StateField, StateEffect} from '@codemirror/state';
 import {syntaxTree} from '@codemirror/language';
 import {getLSP} from '@bhsd/browser';
-import {createTooltipView} from '@bhsd/cm-util';
+import {getSignatureHelpExtension} from '@bhsd/cm-util/cm';
 import {baseData, hoverSelector} from './constants.js';
 import {hoverStyle} from './hover.js';
 import {
@@ -12,7 +10,6 @@ import {
 	findTemplateName,
 	isTemplateParam,
 } from './util.js';
-import type {TooltipView, Tooltip} from '@codemirror/view';
 import type {Extension} from '@codemirror/state';
 import type {
 	SignatureHelp as SignatureHelpBase,
@@ -23,47 +20,6 @@ import type {CodeMirror6} from './codemirror';
 declare interface SignatureHelp extends Omit<SignatureHelpBase, 'signatures'> {
 	signatures: SignatureInformation[] | string[];
 }
-declare interface SignatureEffect {
-	signatureHelp?: SignatureHelp | undefined;
-	text: string;
-	cursor: number;
-}
-
-const signatureEffect = StateEffect.define<SignatureEffect>(),
-	signatureField = StateField.define<SignatureEffect | undefined>({
-		create() {
-			return undefined;
-		},
-		update(oldValue, {state: {doc, selection: {main: {head}}}, effects}) {
-			const text = doc.toString();
-			for (const effect of effects) {
-				if (effect.is(signatureEffect)) {
-					const {value} = effect;
-					if (head === value.cursor && text === value.text) {
-						return value;
-					}
-				}
-			}
-			return oldValue;
-		},
-		provide(f) {
-			return showTooltip.from(f, (value): Tooltip | null => {
-				if (!value) {
-					return null;
-				}
-				const {cursor, signatureHelp} = value;
-				return signatureHelp?.signatures.length
-					? {
-						pos: cursor,
-						above: true,
-						create(view): TooltipView {
-							return createTooltipView(view, getSignatureHelp(signatureHelp), hoverSelector.slice(1));
-						},
-					}
-					: null;
-			});
-		},
-	});
 
 /**
  * @ignore
@@ -90,71 +46,47 @@ export const getSignatureHelp = ({signatures, activeParameter: active}: Signatur
 		return `${safeLabel.slice(0, colon)}:${parts.join('|')}}}`;
 	}).join('<br>');
 
-const dispatchSignatureEffect = (view: EditorView, effect: SignatureEffect): void => {
-	view.dispatch({
-		effects: signatureEffect.of(effect),
-	});
-};
-
 export default (
 	articlePath?: string,
 ) => (
 	cm: CodeMirror6,
-): Extension => {
-	return [
-		signatureField,
-		EditorView.updateListener.of(({view, state, docChanged, selectionSet}) => {
-			if (docChanged || selectionSet && state.field(signatureField)?.signatureHelp?.signatures.length) {
-				const {doc, selection: {main}} = state,
-					{head: cursor} = main,
-					text = doc.toString();
-				if (!main.empty) {
-					dispatchSignatureEffect(view, {text, cursor});
-					return;
+): Extension => [
+	getSignatureHelpExtension<SignatureHelp>({
+		className: hoverSelector.slice(1),
+		render: getSignatureHelp,
+		async update(view, state, {text, cursor}) {
+			cm.lsp ??= getLSP(
+				view,
+				false,
+				{
+					getConfig: toConfigGetter(
+						cm.getWikiConfig,
+						articlePath,
+					),
+					cdn: baseData.CDN,
+				},
+			);
+			const {lsp} = cm;
+			let signatureHelp: SignatureHelp | undefined = await lsp?.provideSignatureHelp(
+				text,
+				indexToPos(state.doc, cursor),
+			);
+			if (!signatureHelp && typeof cm.langConfig?.templateSignature === 'function') {
+				const tree = syntaxTree(state);
+				let node = tree.resolve(cursor, -1);
+				if (node.to === cursor && !isTemplateParam(node)) {
+					node = tree.resolve(cursor, 1);
 				}
-				(async () => {
-					cm.lsp ??= getLSP(
-						view,
-						false,
-						{
-							getConfig: toConfigGetter(
-								cm.getWikiConfig,
-								articlePath,
-							),
-							cdn: baseData.CDN,
-						},
-					);
-					const {lsp} = cm;
-					let signatureHelp: SignatureHelp | undefined = await lsp?.provideSignatureHelp(
-						text,
-						indexToPos(doc, cursor),
-					);
-					if (!signatureHelp && typeof cm.langConfig?.templateSignature === 'function') {
-						const tree = syntaxTree(state);
-						let node = tree.resolve(cursor, -1);
-						if (node.to === cursor && !isTemplateParam(node)) {
-							node = tree.resolve(cursor, 1);
-						}
-						if (isTemplateParam(node)) {
-							const [templateName, parameterName] = findTemplateName(state, node),
-								tooltip = cm.langConfig.templateSignature(templateName, parameterName);
-							if (tooltip) {
-								signatureHelp = {signatures: [tooltip]};
-							}
-						}
+				if (isTemplateParam(node)) {
+					const [templateName, parameterName] = findTemplateName(state, node),
+						tooltip = cm.langConfig.templateSignature(templateName, parameterName);
+					if (tooltip) {
+						signatureHelp = {signatures: [tooltip]};
 					}
-					dispatchSignatureEffect(view, {text, cursor, signatureHelp});
-				})();
-			}
-		}),
-		EditorView.domEventHandlers({
-			keydown({key}, view) {
-				if (key === 'Escape') {
-					const {doc, selection: {main: {head}}} = view.state;
-					dispatchSignatureEffect(view, {text: doc.toString(), cursor: head});
 				}
-			},
-		}),
-		hoverStyle,
-	];
-};
+			}
+			return signatureHelp;
+		},
+	}),
+	hoverStyle,
+];
