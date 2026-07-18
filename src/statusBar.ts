482a926238dee8f16d4dbe0174755f193d341b54
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2026-07-18 22:32:55 +0800

    fix(statusBar): ESLint

--- src/statusBar.ts
+++ src/statusBar.ts
@@ -238,14 +238,15 @@ export default (
 			update({state: {selection: {main}, doc}, transactions, docChanged, selectionSet}): void {
 				for (const tr of transactions) {
 					for (const effect of tr.effects) {
-						if (effect.is(setDiagnosticsEffect)) {
-							diagnostics = effect.value;
-							worker.classList.toggle(workerCls, diagnostics.length > 0);
-							updateDiagnosticsCount(diagnostics, 'error', error);
-							updateDiagnosticsCount(diagnostics, 'warning', warning);
-							updateDiagnosticMessage(view, diagnostics, main, message);
-							updateMenu(cm, diagnostics, main, classList, optionAll, menu, fixer);
+						if (!effect.is(setDiagnosticsEffect)) {
+							continue;
 						}
+						diagnostics = effect.value;
+						worker.classList.toggle(workerCls, diagnostics.length > 0);
+						updateDiagnosticsCount(diagnostics, 'error', error);
+						updateDiagnosticsCount(diagnostics, 'warning', warning);
+						updateDiagnosticMessage(view, diagnostics, main, message);
+						updateMenu(cm, diagnostics, main, classList, optionAll, menu, fixer);
 					}
 				}
 				if (docChanged || selectionSet) {
